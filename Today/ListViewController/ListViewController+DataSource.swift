5469095f8acf0f2db7465dbad9cd82f32d213153
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-01-26 17:29:43 +0200

    Organize view controllers

--- Today/ListViewController/ListViewController+DataSource.swift
+++ Today/ListViewController/ListViewController+DataSource.swift
@@ -5,4 +5,18 @@
 //  Created by Sasha Jaroshevskii on 26.01.2025.
 //
 
-import Foundation
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfig = cell.defaultContentConfiguration()
+        contentConfig.text = reminder.title
+        contentConfig.secondaryText = reminder.dueDate.dateAndTimeText
+        contentConfig.secondaryTextProperties.font = .preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfig
+    }
+}
