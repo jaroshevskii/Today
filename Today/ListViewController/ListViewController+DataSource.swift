d182c56b7c3eeb70fdcc14bd98dde41b0fb37e2b
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-01-26 20:46:55 +0200

    Displaying cell info

--- Today/ListViewController/ListViewController+DataSource.swift
+++ Today/ListViewController/ListViewController+DataSource.swift
@@ -20,7 +20,23 @@ extension ReminderListViewController {
         contentConfig.secondaryTextProperties.font = .preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfig
         
+        var doneButtonConfig = doneButtonConfiguration(for: reminder)
+        doneButtonConfig.tintColor = .todayListCellDoneButtonTint
+        cell.accessories = [
+            .customView(configuration: doneButtonConfig),
+            .disclosureIndicator(displayed: .always),
+        ]
+        
         var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
         backgroundConfig.backgroundColor = .todayListCellBackground
+    }
+    
+    private func doneButtonConfiguration(for reminder: Reminder) -> UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle"
+        let symbolConfig = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfig)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
     }
 }
