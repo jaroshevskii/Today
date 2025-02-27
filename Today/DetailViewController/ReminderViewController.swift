5ce917d848727dfb053a279ade0b663e6ba740d4
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-02-27 21:24:52 +0200

    Create an enumeration for rows

--- Today/DetailViewController/ReminderViewController.swift
+++ Today/DetailViewController/ReminderViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource?
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -20,5 +23,33 @@ class ReminderViewController: UICollectionViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHeandler)
+        dataSource = DataSource(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    func cellRegistrationHeandler(cell: UICollectionViewListCell, indexPaht: IndexPath, row: Row) {
+        var contentConfig = cell.defaultContentConfiguration()
+        contentConfig.text = text(for: row)
+        contentConfig.textProperties.font = .preferredFont(forTextStyle: row.textStyle)
+        contentConfig.image = row.image
+        cell.contentConfiguration = contentConfig
+        cell.tintColor = .todayPrimaryTint
+    }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title: return reminder.title
+        }
     }
 }
