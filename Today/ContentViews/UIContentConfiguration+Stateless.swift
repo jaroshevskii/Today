e3615985e9f1a06aa9a7af1ab99a5babb133d026
jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-03-08 12:53:08 +0200

    Conform to the content view protocol

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Tamila Leschcenko on 08.03.2025.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
